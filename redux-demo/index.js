08c1a20ce90aece1f8ae64f1cf54e209ee149f9f
unknown <saraswathi.mac@gmail.com>
2022-05-12 12:51:29 +0530

    Reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -7,3 +7,21 @@ function orderCake() {
         quantity: 1,
     }
 }
+
+const initialState = {
+    numOfCakes: 10,
+    anotherProperty: 0
+}
+
+// Reducers: (previousState, action) => newState
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case CAKE_ORDERD: 
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1,
+            }
+            default:
+                return state;
+    }
+}
